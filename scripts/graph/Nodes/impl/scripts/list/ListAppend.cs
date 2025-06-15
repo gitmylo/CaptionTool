a19e0b4fa0c3fae4ac834881bfbbd560ff1d3980
Mylo <36931363+gitmylo@users.noreply.github.com>
2025-06-15 16:27:39 +0200

    List append for multi-message chats etc

---
+++ scripts/graph/Nodes/impl/scripts/list/ListAppend.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Godot;
+using Godot.Collections;
+
+namespace CaptionTool.scripts.graph.Nodes.impl.scripts.list;
+
+[GlobalClass]
+public partial class ListAppend : ExecutionCore
+{
+    public override async Task<Array<Array>> Execute(Array<Array> inputs, NodeExecutionContext context, Array values)
+    {
+        var list = inputs[0].FromUGdArray<Variant>().ToList();
+        list.AddRange(inputs[1]);
+        return Results(list.ToUGdArray());
+    }
+}
